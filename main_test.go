0f05a8cb5afd6a6083fe2cffa90f80a749eca475
Reggie Davis <reginald.davis88@gmail.com>
2019-07-12 01:16:16 -0500

    Added mock server and feed data

--- main_test.go
+++ main_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -9,6 +11,31 @@ const (
 	checkMark = "/u02713"
 	ballotX   = "/u2717"
 )
+
+var feed = `<?xml version="1.0" encoding="UTF-8"?>
+	<rss>
+	<channel>
+		<title>Going Go Programming</title>
+		<description>Golang : https://github.com/goinggo</description>
+		<link>http://www.goinggo.net/</link>
+		<item>
+			<pubDate>Sun, 15 Mar 2015 15:04:00 +0000</pubDate>
+			<title>Object Oriented Programming Mechanics</title>
+			<description>Go is an object oriented language.</description>
+			<link>http://www.goinggo.net/2015/03/object-oriented</link>
+		</item>
+	</channel>
+	</rss>`
+
+func mockServer() *httptest.Server {
+	f := func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Header().Set("Content-Type", "application/xml")
+		fmt.Fprintf(w, feed)
+	}
+
+	return httptest.NewServer(http.HandlerFunc(f))
+}
 
 func TestDownload(t *testing.T) {
 
